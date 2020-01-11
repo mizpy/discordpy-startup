b202b5459cddb34d1f21eb6dbac4cf6106655892
mizpy <59769810+mizpy@users.noreply.github.com>
2020-01-12 03:27:45 +0900

    edit

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,34 @@
 from discord.ext import commands
+from datetime import datetime
+from discord.ext import tasks
 import os
 import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+channel_id = os.environ['DISCORD_CHANNEL_ID']
 
+#@bot.event
+#async def on_command_error(ctx, error):
+#    orig_error = getattr(error, "original", error)
+#    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+#    await ctx.send(error_msg)
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+#@bot.command()
+#async def ping(ctx):
+#    await ctx.send('pong')
 
+# 60秒に一回ループ
+@tasks.loop(seconds=60)
+async def loop():
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    print(now)
+    if now == '03:35':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('テスト投稿です！')  
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
+#ループ処理実行
+loop.start()
 
 bot.run(token)
