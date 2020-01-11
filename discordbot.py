bf5ee3683f73fcc60c76c3852ba5be98ce02f730
mizpy <59769810+mizpy@users.noreply.github.com>
2020-01-12 04:55:55 +0900

    edit

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,9 @@ import discord
 from datetime import datetime
 from discord.ext import tasks
 
-TOKEN = "NjY1NTg0MTYzMjQzNjg3OTU2.XhoCIw.Lqq_vyi8twCEoZLvHceF7n_02Vg" #トークン
+logged = 0
+
+TOKEN = "NjY1NTg0MTYzMjQzNjg3OTU2.XhohOQ.gg9rtHy-mkFPO1k5fcQszWwEyQQ" #トークン
 CHANNEL_ID = 665527939160473622 #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
@@ -14,6 +16,9 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    global logged
+    logged = 1
+    print(logged)
     
 # 60秒に一回ループ
 @tasks.loop(seconds=60)
@@ -21,11 +26,15 @@ async def loop():
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
     print(now)
-#    if now == '02:21':
-     channel = client.get_channel(CHANNEL_ID)
-     await channel.send('テスト投稿です！')  
+    print(logged)
+    #if now == '02:21':
+    if logged == 1:
+        print('send')
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('テスト投稿です！')  
 
 #ループ処理実行
 loop.start()
+
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
